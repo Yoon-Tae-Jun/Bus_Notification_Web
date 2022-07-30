221b5cf954ac737f819b1cc7e4804df68684a5d9
Yoon-Tae-Jun <20214004@sch.ac.kr>
2022-07-30 17:03:10 +0900

    change color, font size

--- src/Tabs/DOWN/accordion_body.js
+++ src/Tabs/DOWN/accordion_body.js
@@ -3,14 +3,14 @@ function Accordion_body({bus_time, cycle, term, arrive_time }){
     const date = new Date();
     return(
         <div >
-            <h4> 셔틀버스 시간: { 
+            <h4> 셔틀버스: { 
                 (function() {
                     if (bus_time === null) return "없음";
                     else if (arrive_time !==null) {
                         date.setMinutes(date.getMinutes()+Number(term) + Number(arrive_time))
                         const time = ('0' + date.getHours()).slice(-2) + ":" + ('0' + date.getMinutes()).slice(-2);
                         return ( time +" 출발 예정(간격: "+term+"분)");}
-                    else return (bus_time+" 출발 예정(간격: "+term+"분")
+                    else return (bus_time+" 출발 예정(간격: "+term+"분)")
                 })()
             }
              </h4>
