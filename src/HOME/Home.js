4041f2aa16a2012cc4966f510b9cc00be1b0e024
Yoon-Tae-Jun <20214004@sch.ac.kr>
2022-08-17 02:52:57 +0900

    renewal home

--- src/HOME/Home.js
+++ src/HOME/Home.js
@@ -1,55 +1,56 @@
 import { useState, useEffect } from "react";
-import { Accordion } from "react-bootstrap";
-import DownContent from "./DOWN/accordion_content";
-import UpContent from "./UP/accordion_content";
+
 import styles from "./Home.module.css";
 function Home(){
-    const [down_time, setDownTime] = useState([]);
-    const [up_time, setUpTime] = useState([]);
-    const [nowtime, setNowTime] = useState(getTime());
-    const IP = "43.200.27.101"
+    const [time, setTime] = useState([]);
+    const IP = "127.0.0.1"
 
-    async function getDownTime(){
-        const response = await fetch("http://" + IP + `:8080/subway/running`);
+    async function getTime(){
+        const response = await fetch("http://" + IP + `:8080/subway/all`);
           const json = await response.json();
-          setDownTime(json.data)
+          setTime(json.data)
       };
 
-    async function getUpTime(limit){
-        const response = await fetch("http://" + IP + `:8080/subway/current/${limit}`);
-          const json = await response.json();
-          setUpTime(json.data)
-      };
+      
     useEffect(() => {
-        getDownTime();
-        getUpTime(3);
-        const id = setInterval(() => {
-            setNowTime(getTime());
-          }, 1000);
-          return (() => clearInterval(id))
+        getTime();
     },[]);
 
     return(
         <div className={styles.homeContainer}>
-            <div className={styles.time}>현재 시간: {nowtime}</div>
-            <div className={styles.downtitle}>등교</div>
-            <Accordion defaultActiveKey="0">
-                <DownContent 
-                times={down_time} />
-            </Accordion>
-            <div className={styles.uptitle}>하교</div>
-            <Accordion defaultActiveKey="0">
-            <UpContent
-            times={up_time}/>
-            </Accordion>
-
-
+            {console.log(GetUpTime(time))}
+            <div>등교</div>
+            <div>하교</div>
+            <div>즐겨찾기</div>
         </div>
     )
 }
 
-function getTime(){
+function GetUpTime(times) {
     let today = new Date();  
-    return ( ('0' + today.getHours()).slice(-2) + ":" +('0' + today.getMinutes()).slice(-2) + ":" + ('0' + today.getSeconds()).slice(-2));
+    
+    for (var time of times){
+        // down일 경우 continue
+        if (time["updown"] === "2"){
+            continue;
+        }
+
+        // time에서 시, 분 추출
+        var hour = Number(time["train_TIME"].split(":")[0]);
+        var min = Number(time["train_TIME"].split(":")[1]);
+        
+        // 시간이 지났으면 continue
+        if (today.getHours() > hour){
+            continue;
+        }
+
+        // 분이 아직 안지났다면 시간 return
+        if (today.getMinutes() < min){
+            return time;
+        }
+    }
+
 }
+
+
 export default Home;
